6fbf967b99a16667102d835f0f0245255e890d17
Daniel Roe <daniel@roe.dev>
2022-07-16 22:58:32 +0100

    docs: add jsdocs for inputs

--- src/core/inputs.ts
+++ src/core/inputs.ts
@@ -1,17 +1,27 @@
 import { createInput } from './internal'
 
 export interface Input<T extends string = never> {
+  /** this adds a new pattern to the current input */
   and: <X extends string = never>(input: string | Input<X>) => Input<T | X>
+  /** this provides an alternative to the current input */
   or: <X extends string = never>(input: string | Input<X>) => Input<T | X>
+  /** this is a positive lookbehind. Make sure to check [browser support](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#browser_compatibility) as not all browsers support lookbehinds (notably Safari) */
   after: (input: string | Input) => Input<T>
+  /** this is a positive lookahead */
   before: (input: string | Input) => Input<T>
+  /** these is a negative lookbehind. Make sure to check [browser support](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#browser_compatibility) as not all browsers support lookbehinds (notably Safari) */
   notAfter: (input: string | Input) => Input<T>
+  /** this is a negative lookahead */
   notBefore: (input: string | Input) => Input<T>
   times: {
+    /** repeat the previous pattern an exact number of times */
     (number: number): Input<T>
+    /** specify a range of times to repeat the previous pattern */
     between: (min: number, max: number) => Input<T>
   }
+  /** this defines the entire input so far as a named capture group. You will get type safety when using the resulting RegExp with `String.match()` */
   as: <K extends string>(key: K) => Input<T | K>
+  /** this allows you to match beginning/ends of lines with `at.lineStart()` and `at.lineEnd()` */
   at: {
     lineStart: () => Input<T>
     lineEnd: () => Input<T>
@@ -19,8 +29,11 @@ export interface Input<T extends string = never> {
   toString: () => string
 }
 
+/** This matches any character in the string provided */
 export const charIn = (chars: string) => createInput(`[${chars.replace(/[-\\^\]]/g, '\\$&')}]`)
+/** This matches any character that is not in the string provided */
 export const charNotIn = (chars: string) => createInput(`[^${chars.replace(/[-\\^\]]/g, '\\$&')}]`)
+/** This takes an array of inputs and matches any of them. */
 export const anyOf = <T extends string = never>(...args: Array<string | Input<T>>) =>
   createInput<T>(`(${args.map(a => exactly(a)).join('|')})`)
 
@@ -43,6 +56,8 @@ export const not = {
   carriageReturn: createInput('[^\\r]'),
 }
 
+/** Equivalent to `?` - this marks the input as optional */
 export const maybe = (str: string | Input) => createInput(`(${exactly(str)})?`)
+/** This escapes a string input to match it exactly */
 export const exactly = (str: string | Input) =>
   typeof str === 'string' ? createInput(str.replace(/[.*+?^${}()|[\]\\/]/g, '\\$&')) : str
