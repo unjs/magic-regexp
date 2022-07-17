7fe1703a489b35e4c52e58c9b48682a78df88413
Daniel Roe <daniel@roe.dev>
2022-07-17 18:40:40 +0100

    docs: update playground with a better example

--- playground/index.mjs
+++ playground/index.mjs
@@ -1,12 +1,19 @@
 import assert from 'node:assert'
-import { createRegExp, exactly, digit } from 'magic-regexp'
+import { createRegExp, exactly, digit, oneOrMore, char } from 'magic-regexp'
 
-const regExp = createRegExp(exactly('foo/test.js').after('bar/'))
-assert.equal(regExp.test('bar/foo/test.js'), true)
+// Typed capture groups
+const ID_RE = createRegExp(exactly('id-').and(digit.times(5).as('id')))
+const groups = 'some id-23490 here we go'.match(ID_RE)?.groups
+console.log(ID_RE, groups?.id)
 
-console.log(regExp)
+// Quick-and-dirty semver
+const SEMVER_RE = createRegExp(
+  oneOrMore(digit)
+    .as('major')
+    .and('.')
+    .and(oneOrMore(digit).as('minor'))
+    .and(exactly('.').and(oneOrMore(char).as('patch')).optionally())
+)
+console.log(SEMVER_RE)
 
-const typedCaptureGroup = createRegExp(exactly('id-').and(digit.times(5).as('id')))
-const groups = 'some id-23490 here we go'.match(typedCaptureGroup)?.groups
-
-console.log(typedCaptureGroup, groups?.id)
+assert.equal(createRegExp(exactly('foo/test.js').after('bar/')).test('bar/foo/test.js'), true)
