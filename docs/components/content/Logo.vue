f058426c134013b5df9e4281ffac65ef1dd68112
Daniel Roe <daniel@roe.dev>
2022-07-19 16:43:12 +0100

    docs: add initial documentation site (#14)

---
+++ docs/components/content/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="font-bold flex flex-row items-center gap-2">🦄 magic-regexp</div>
+</template>
--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,ts,mjs,cjs}': ['pnpm lint:eslint', 'pnpm lint:prettier'],
-  '{!(package)*.json,*.code-snippets,.*rc}': ['pnpm lint:prettier --parser json'],
+  '{!(package)*.json,*.code-snippets,.!(npm)*rc}': ['pnpm lint:prettier --parser json'],
   'package.json': ['pnpm lint:prettier'],
 }
