6cbb1412efa1d1296d5de21f98531549d18345e1
Daniel Roe <daniel@roe.dev>
2022-07-24 15:18:51 +0100

    feat(types): respect global flag for .match and .matchAll types

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,15 @@ export * from './core/types/magic-regexp'
 // Add additional overload to global String object types to allow for typed capturing groups
 declare global {
   interface String {
-    match<R extends MagicRegExp<string, string, string>>(regexp: R): MagicRegExpMatchArray<R> | null
+    match<R extends MagicRegExp<string, string, Exclude<Flag, 'g'>>>(
+      regexp: R
+    ): MagicRegExpMatchArray<R> | null
+    match<R extends MagicRegExp<string, string, 'g'>>(regexp: R): string[] | null
+
+    /** @deprecated String.matchAll requires global flag to be set */
+    matchAll<R extends MagicRegExp<string, string, never>>(regexp: R): never
+    /** @deprecated String.matchAll requires global flag to be set*/
+    matchAll<R extends MagicRegExp<string, string, Exclude<Flag, 'g'>>>(regexp: R): never
 
     matchAll<R extends MagicRegExp<string, string, string>>(
       regexp: R
