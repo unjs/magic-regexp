8aa43e80781c86e739e7491e8396358cdc88cdb0
Daniel Roe <daniel@roe.dev>
2022-08-03 19:05:24 +0100

    build: emit cjs

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,7 @@
 import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   declaration: true,
+  rollup: { emitCJS: true },
   entries: ['./src/index', './src/transform'],
   externals: ['magic-regexp'],
 })
