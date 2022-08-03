b2124c096a406cd6c5b74418ad4bfe51b79fcdec
Daniel Roe <daniel@roe.dev>
2022-08-03 19:12:17 +0100

    build: emit cjs (#44)

--- transform.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { join } = require('path')
-const jiti = require('jiti')
-
-// @ts-ignore
-module.exports = jiti(null, { interopDefault: true, esmResolve: true })(
-  join(__dirname, './dist/transform.mjs')
-)
