90f809c2fb1e9aea697652a71ee374534e055aeb
David Tai <didavid61202@hotmail.com>
2023-01-29 18:02:12 +0800

    update `createRegExp` function

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,31 @@ import { Input, exactly } from './core/inputs'
 import type { Join } from './core/types/join'
 import type { MagicRegExp, MagicRegExpMatchArray } from './core/types/magic-regexp'
 
-export const createRegExp = <
-  Value extends string,
-  NamedGroups extends string = never,
-  CapturedGroupsArr extends (string | undefined)[] = [],
-  Flags extends Flag[] = never[]
->(
-  raw: Input<Value, NamedGroups, CapturedGroupsArr> | Value,
-  flags?: [...Flags] | string | Set<Flag>
-) =>
-  new RegExp(exactly(raw).toString(), [...(flags || '')].join('')) as MagicRegExp<
-    `/${Value}/${Join<Flags, '', ''>}`,
-    NamedGroups,
-    CapturedGroupsArr,
+import type { Escape, ExactEscapeChar } from './core/types/escape'
+
+export const createRegExp: {
+  /** Create Magic RegExp from Input helper */
+  <
+    Value extends string,
+    NamedGroups extends string = never,
+    CapturedGroupsArr extends (string | undefined)[] = [],
+    Flags extends Flag[] = never[]
+  >(
+    raw: Input<Value, NamedGroups, CapturedGroupsArr>,
+    flags?: [...Flags] | string | Set<Flag>
+  ): MagicRegExp<`/${Value}/${Join<Flags, '', ''>}`, NamedGroups, CapturedGroupsArr, Flags[number]>
+  /** Create Magic RegExp from string, string will be sanitized */
+  <Value extends string, Flags extends Flag[] = never[]>(
+    raw: Value,
+    flags?: [...Flags] | string | Set<Flag>
+  ): MagicRegExp<
+    `/${Escape<Value, ExactEscapeChar>}/${Join<Flags, '', ''>}`,
+    never,
+    [],
     Flags[number]
   >
+} = (raw: any, flags?: any) =>
+  new RegExp(exactly(raw).toString(), [...(flags || '')].join('')) as any
 
 export * from './core/flags'
 export * from './core/inputs'
