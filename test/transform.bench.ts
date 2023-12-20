02f68c8499f36c44e6bbe5575e0a4f829152b159
Daniel Roe <daniel@roe.dev>
2023-12-20 20:36:39 +0000

    refactor: split out separate benchmarks

--- test/transform.bench.ts
+++ test/transform.bench.ts
@@ -3,38 +3,56 @@ import { parse } from 'acorn'
 
 import { MagicRegExpTransformPlugin } from '../src/transform'
 
-const testCases = [
-  [
-    `import { createRegExp, exactly, anyOf } from 'magic-regexp'`,
-    "const re1 = createRegExp(exactly('bar').notBefore('foo'))",
-  ],
-  "const re1 = createRegExp(exactly('bar').notBefore('foo'))",
-  [`// magic-regexp`, "const re1 = createRegExp(exactly('bar').notBefore('foo'))"],
-  [
-    "import { something } from 'other-module'",
-    `import { createRegExp, exactly, anyOf } from 'magic-regexp'`,
-    '//', // this lets us tree-shake the import for use in our test-suite
-    "const re1 = createRegExp(exactly('bar').notBefore('foo'))",
-    "const re2 = createRegExp(anyOf(exactly('bar'), 'foo'))",
-    "const re3 = createRegExp('/foo/bar')",
-    // This line will be double-escaped in the snapshot
-    "re3.test('/foo/bar')",
-  ],
-  [
-    `import { createRegExp as cRE } from 'magic-regexp'`,
-    `import { exactly as ext, createRegExp } from 'magic-regexp'`,
-    `import * as magicRE from 'magic-regexp'`,
-    "const re1 = cRE(ext('bar').notBefore('foo'))",
-    "const re2 = magicRE.createRegExp(magicRE.anyOf('bar', 'foo'))",
-    "const re3 = createRegExp('test/value')",
-  ],
-]
-
 describe(`transformer: magic-regexp`, () => {
-  bench('transformer: magic-regexp', () => {
-    for (const testCase of testCases) {
-      transform(testCase)
-    }
+  const couldTransform = [
+    `import { createRegExp, exactly, anyOf } from 'magic-regexp'`,
+    "const re1 = createRegExp(exactly('bar').notBefore('foo'))",
+  ]
+
+  bench('ignores non-JS files', () => {
+    transform(couldTransform, 'test.css')
+  })
+
+  bench('transforms vue script blocks', () => {
+    transform(couldTransform, 'test.vue?type=script')
+    transform(couldTransform, 'test.vue')
+    transform(couldTransform, 'test.vue?type=template')
+  })
+
+  bench(`ignores code without imports from magic-regexp`, () => {
+    transform(couldTransform[1])
+    transform([`// magic-regexp`, couldTransform[1]])
+  })
+
+  bench('preserves context for dynamic regexps', () => {
+    transform([
+      `import { createRegExp } from 'magic-regexp'`,
+      `console.log(createRegExp(anyOf(keys)))`,
+    ])
+  })
+
+  bench('statically replaces regexps where possible', () => {
+    transform([
+      "import { something } from 'other-module'",
+      `import { createRegExp, exactly, anyOf } from 'magic-regexp'`,
+      '//', // this lets us tree-shake the import for use in our test-suite
+      "const re1 = createRegExp(exactly('bar').notBefore('foo'))",
+      "const re2 = createRegExp(anyOf(exactly('bar'), 'foo'))",
+      "const re3 = createRegExp('/foo/bar')",
+      // This line will be double-escaped in the snapshot
+      "re3.test('/foo/bar')",
+    ])
+  })
+
+  bench('respects how users import library', () => {
+    transform([
+      `import { createRegExp as cRE } from 'magic-regexp'`,
+      `import { exactly as ext, createRegExp } from 'magic-regexp'`,
+      `import * as magicRE from 'magic-regexp'`,
+      "const re1 = cRE(ext('bar').notBefore('foo'))",
+      "const re2 = magicRE.createRegExp(magicRE.anyOf('bar', 'foo'))",
+      "const re3 = createRegExp('test/value')",
+    ])
   })
 })
 
