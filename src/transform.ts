5d8bba5f0e3589f6478004533ea1d09147c8338f
Alexander Lichter <github@lichter.io>
2026-09-18 08:24:17 +0200

    perf(transform): add plugin hook filter (#753)

--- src/transform.ts
+++ src/transform.ts
@@ -51,86 +51,91 @@ export const MagicRegExpTransformPlugin = createUnplugin(() => {
 
       return false
     },
-    transform(code, id) {
-      if (!code.includes('magic-regexp'))
-        return
-
-      const ast = this.parse(code) as unknown as Program
-
-      const contextMap: Context = { ...magicRegExp }
-      const wrapperNames: string[] = []
-      let namespace: string
-      let hasRelevantImport = false
-
-      walkAST(ast, (node) => {
-        if (node.type !== 'ImportDeclaration')
+    transform: {
+      filter: {
+        code: 'magic-regexp',
+      },
+      handler(code, id) {
+        if (!code.includes('magic-regexp'))
           return
 
-        if (typeof node.source.value !== 'string' || !MAGIC_REGEXP_SPECIFIERS.has(node.source.value))
-          return
+        const ast = this.parse(code) as unknown as Program
 
-        hasRelevantImport = true
+        const contextMap: Context = { ...magicRegExp }
+        const wrapperNames: string[] = []
+        let namespace: string
+        let hasRelevantImport = false
 
-        for (const spec of node.specifiers) {
-          if (spec.type === 'ImportNamespaceSpecifier') {
-            namespace = spec.local.name
-            contextMap[spec.local.name] = magicRegExp
+        walkAST(ast, (node) => {
+          if (node.type !== 'ImportDeclaration')
+            return
+
+          if (typeof node.source.value !== 'string' || !MAGIC_REGEXP_SPECIFIERS.has(node.source.value))
+            return
+
+          hasRelevantImport = true
+
+          for (const spec of node.specifiers) {
+            if (spec.type === 'ImportNamespaceSpecifier') {
+              namespace = spec.local.name
+              contextMap[spec.local.name] = magicRegExp
+            }
+            else if (spec.type === 'ImportSpecifier') {
+              const importedName = spec.imported.type === 'Identifier' ? spec.imported.name : String(spec.imported.value)
+              if (importedName in magicRegExp)
+                contextMap[spec.local.name] = magicRegExp[importedName as keyof typeof magicRegExp]
+              if (importedName === 'createRegExp')
+                wrapperNames.push(spec.local.name)
+            }
           }
-          else if (spec.type === 'ImportSpecifier') {
-            const importedName = spec.imported.type === 'Identifier' ? spec.imported.name : String(spec.imported.value)
-            if (importedName in magicRegExp)
-              contextMap[spec.local.name] = magicRegExp[importedName as keyof typeof magicRegExp]
-            if (importedName === 'createRegExp')
-              wrapperNames.push(spec.local.name)
+        })
+
+        if (!hasRelevantImport)
+          return
+
+        const context = createContext(contextMap)
+
+        const s = new MagicString(code)
+
+        walkAST(ast, (node) => {
+          if (node.type !== 'CallExpression')
+            return
+
+          const { callee } = node
+          const isDirectCall = callee.type === 'Identifier'
+            && wrapperNames.includes(callee.name)
+
+          let isNamespacedCall = false
+          if (callee.type === 'MemberExpression') {
+            const { object, property } = callee
+            isNamespacedCall = object.type === 'Identifier'
+              && object.name === namespace
+              && property.type === 'Identifier'
+              && property.name === 'createRegExp'
+          }
+
+          if (!isDirectCall && !isNamespacedCall)
+            return
+
+          const { start, end } = node as NodeWithPosition
+
+          try {
+            const value = runInContext(code.slice(start, end), context)
+            s.overwrite(start, end, value.toString())
+          }
+          catch {
+            // We silently ignore any code that relies on external context
+            // as it can use runtime `magic-regexp` support
+          }
+        })
+
+        if (s.hasChanged()) {
+          return {
+            code: s.toString(),
+            map: s.generateMap({ includeContent: true, source: id }),
           }
         }
-      })
-
-      if (!hasRelevantImport)
-        return
-
-      const context = createContext(contextMap)
-
-      const s = new MagicString(code)
-
-      walkAST(ast, (node) => {
-        if (node.type !== 'CallExpression')
-          return
-
-        const { callee } = node
-        const isDirectCall = callee.type === 'Identifier'
-          && wrapperNames.includes(callee.name)
-
-        let isNamespacedCall = false
-        if (callee.type === 'MemberExpression') {
-          const { object, property } = callee
-          isNamespacedCall = object.type === 'Identifier'
-            && object.name === namespace
-            && property.type === 'Identifier'
-            && property.name === 'createRegExp'
-        }
-
-        if (!isDirectCall && !isNamespacedCall)
-          return
-
-        const { start, end } = node as NodeWithPosition
-
-        try {
-          const value = runInContext(code.slice(start, end), context)
-          s.overwrite(start, end, value.toString())
-        }
-        catch {
-          // We silently ignore any code that relies on external context
-          // as it can use runtime `magic-regexp` support
-        }
-      })
-
-      if (s.hasChanged()) {
-        return {
-          code: s.toString(),
-          map: s.generateMap({ includeContent: true, source: id }),
-        }
-      }
+      },
     },
   }
 })
